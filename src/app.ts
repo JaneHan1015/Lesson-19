9d2860d4df15c68a84d8a5c7b128405cb9463155
janehan1015 <janehan1015@gmail.com>
2024-01-18 17:00:47 +0900

    init

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import session from "express-session";
-import { createMeeting, checkLogin, createPreferredTime, finalDecision, getMeetingInfo, changeMeeting } from "./db";
+import {
+  changeMeeting,
+  checkLogin,
+  createMeeting,
+  createPreferredTime,
+  finalDecision,
+  getMeetingInfo,
+} from "./db";
+import mysql from "mysql2";
+import "dotenv/config";
 
 declare module "express-session" {
   interface SessionData {
-    authenticated? boolean;
-    user? {
-      name: string,
-      role:
-    }
+    authenticated?: boolean;
+    user?: {
+      name: string;
+      role: string;
+      meetingId: number;
+    };
   }
 }
 
@@ -18,48 +28,71 @@ const app = express();
 const corsOptions = {
   origin: "*",
 };
- 
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DATABASE,
+  password: process.env.DB_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+  ssl: {
+    rejectUnauthorized: true,
+  },
+});
+const promisePool = pool.promise();
+
 app.use(
   session({
-  secret: 'veryverysecurecode',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 30000 },
-})
+    secret: "veryverysecurecode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 300000 },
+  })
 );
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
-app.get("./hello", (req, res) => {
-  //Do whatever you want
+app.get("/hello", async (req, res) => {
+  // Do whatever you want
+  const [rows, _] = await promisePool.query("SELECT id, title FROM meeting");
+  console.log(rows);
   return res.status(200).send({
-    test: "gooood..",
+    rows: rows,
   });
 });
 
-app.post("/createMeeting", (req, res) => {
-  const meetingId = createMeeting(
+app.post("/createMeeting", async (req, res) => {
+  const meetingId = await createMeeting(
+    promisePool,
     req.body.title,
     req.body.description,
     req.body.timezone,
+    req.body.meetingLength,
     req.body.hostName,
+    req.body.hostPassword,
     req.body.hostPreferredTime
   );
   return res.status(200).send({
-    id: meetingId,
+    meetingId: meetingId,
   });
 });
 
-app.get("/:meetingId/meetingInfo", (req, res) => {
+app.get("/:meetingId/meetingInfo", async (req, res) => {
   const meetingId = req.params.meetingId;
-  const meetingInfo = getMeetingInfo(parseInt(meetingId));
+  const meetingInfo = await getMeetingInfo(promisePool, parseInt(meetingId));
 
   return res.status(200).send(meetingInfo);
 });
 
-app.post("/:meetingId/preferredTime", (req, res) => {
+app.post("/:meetingId/preferredTime", async (req, res) => {
   const meetingId = req.params.meetingId;
-  createPreferredTime(
+  await createPreferredTime(
+    promisePool,
     parseInt(meetingId),
     req.body.name,
     req.body.preferredTime
@@ -69,17 +102,31 @@ app.post("/:meetingId/preferredTime", (req, res) => {
 
 app.put("/:meetingId/changeMeeting", (req, res) => {
   const meetingId = req.params.meetingId;
-  changeMeeting(
-    parseInt(meetingId),
-    req.body.title,
-    req.body.description,
-    req.body.timezone,
-    req.body.hostName,
-    req.body.hostPreferredTime
-  );
-  return res.status(200).send({
-    id: meetingId,
-  });
+
+  if (
+    req.session.authenticated == true &&
+    req.session.user &&
+    req.session.user.meetingId == parseInt(meetingId) &&
+    req.session.user.role == "host"
+  ) {
+    // TODO Change meeting table on SQL
+    changeMeeting(
+      parseInt(meetingId),
+      req.body.title,
+      req.body.description,
+      req.body.timezone,
+      req.body.meetingLength,
+      req.body.hostName,
+      req.body.hostPreferredTime
+    );
+    return res.status(200).send({
+      id: meetingId,
+    });
+  } else {
+    return res.status(403).send({
+      error: "Bad authentication",
+    });
+  }
 });
 
 app.post("/:meetingId/finalDecision", (req, res) => {
@@ -88,52 +135,38 @@ app.post("/:meetingId/finalDecision", (req, res) => {
   return res.status(201).send();
 });
 
-app.post("/:meetingId/login", (req,rees) => {
+app.post("/:meetingId/login", async (req, res) => {
   const meetingId = req.params.meetingId;
   const { user, password } = req.body;
-  console.log(req.sessionID);
+
   if (user && password) {
     if (req.session.authenticated) {
-      return resizeBy.status(201).send(req.session);
+      return res.status(201).send({ status: "Logged in" });
     } else {
-      if (checkLogin(parseInt(meetingId), user, password)) {
+      if (await checkLogin(promisePool, parseInt(meetingId), user, password)) {
         req.session.authenticated = true;
         req.session.user = {
           name: user,
-          role: "host"
+          role: "host",
+          meetingId: parseInt(meetingId),
         };
-        return res.status(201).send(req.session);
-        return res.status(403).send({ error: "Bad authentication"});
+        return res.status(201).send({ status: "Logged in" });
+      } else {
+        return res.status(403).send({ error: "Bad authentication" });
       }
-    } else {
-      return res.status(403).send({ error: "Bad authentication"})
-    }
-    }
-
-    if (checkLogin(parseInt(meetingId), user, password)) {
-      req.session.authenticated = true;
-      req.session.user = {
-        name: user,
-        role: "host"
-      };
-      return res.status(201).send(req.session);
-      return res.status(403).send({ error: "Bad authentication"});
     }
   } else {
-    return res.status(403).send({ error: "Bad authentication"})
+    return res.status(403).send({ error: "Bad authentication" });
   }
-  // 3. Id, pw check -> In the DB?
-  // 4. IF true -> create session -> return cookie
-  // 5. If false -> return error
 });
 
 app.get("/checkAuthed", (req, res) => {
   if (req.session.authenticated == true) {
-    return res.status(200).send({status: "Logged in!.  Good..."});
+    return res.status(200).send({ status: "Logged in!. Good..." });
   } else {
-    return res.status(403).send({ status: "Not logged in.  Too bad..."});
+    return res.status(403).send({ status: "Not logged in. Too bad..." });
   }
-})
+});
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
--- src/db.ts
+++ src/db.ts
@@ -1,57 +1,116 @@
-export function createMeeting(
+import { Pool } from "mysql2/promise";
+
+export async function createMeeting(
+  pool: Pool,
   title: string,
   description: string,
   timezone: string,
+  meetingLength: number,
   hostName: string,
+  hostPassword: string,
   preferredTime: number[]
 ) {
-  // TODO Send SDL to the DB
-  return 32;
+  // 1. Insert meeting
+  const [rows, _] = await pool.execute(
+    "INSERT INTO meeting (title, description, timezone, meeting_length) VALUES (?, ?, ?, ?)",
+    [title, description, timezone, meetingLength]
+  );
+  const meetingId = (rows as any).insertId;
+
+  // 2. Insert host
+  const [hrows, __] = await pool.execute(
+    "INSERT INTO host (name, password, meeting_id, preferred_time) VALUES (?, ?, ?, ?)",
+    [hostName, hostPassword, meetingId, preferredTime]
+  );
+
+  return meetingId;
 }
 
-export function changeMeeting(
+export async function changeMeeting(
+  pool: Pool,
   meetingId: number,
   title: string,
   description: string,
   timezone: string,
+  meetingLength: number,
   hostName: string,
   preferredTime: number[]
 ) {
-  // TODO Send SDL to the DB
+  // TODO Send SQL to the DB
+  const [mrows, _] = await pool.execute(
+    "UPDATE meeting SET title = ?, description = ?, timezone = ?, meeting_length = ? Where id = ?",
+    [title, description, timezone, meetingLength, meetingId]
+  );
+
+  //host --> id, name, password, preferred_time, meeting_id
+  const [hrows, __] = await pool.execute(
+    "UPDATE host SET name = ?, preferred_time = ? WHERE meeting_id = ?,"[
+      (hostName, preferredTime)
+    ]
+  );
+
   return;
 }
 
-export function getMeetingInfo(id: number) {
-  //TODO Find meeting info using the id
+export async function getMeetingInfo(pool: Pool, id: number) {
+  // 1. Get meeting info from meeting table
+  const [rows, _] = await pool.execute("SELECT * FROM meeting WHERE id = ?", [
+    id,
+  ]);
+  const meeting = (rows as any)[0];
+
+  // 2. Get host info from host table
+  const [hrows, __] = await pool.execute(
+    "SELECT name, preferred_time FROM host WHERE meeting_id = ?",
+    [id]
+  );
+  const host = (hrows as any)[0];
+
+  // TODO 3. Get guest info from guest table
+
   return {
-    id: id,
-    title: "My meeting",
-    description: "This meeting is only for testing..",
-    timezone: "UTS",
-    hostName: "Gavin",
+    ...meeting,
+    host: {
+      ...host,
+    },
   };
 }
 
-export function createPreferredTime(
+export async function createPreferredTime(
+  pool: Pool,
   meetingId: number,
   name: string,
   preferredTime: number[]
 ) {
   // TODO Send SQL query to insert preferred time data to the meeting
+  // 1. Insert guest data into guest table
+  const [rows, _] = await pool.execute(
+    "INSERT INTO guest (name, meeting_id, preferred_time) VALUES (?, ?, ?)",
+    [name, meetingId, preferredTime]
+  );
   return;
-}
-
-
-export function checkLogin(meetingId: number, user: string, password: string) {
-  //TODO Check SQL
-  if (user == "gavin" && password == "1234") {
-    return true,
-  } else {
-    return false;
-  }
 }
 
 export function finalDecision(meetingId: number, decidedTime: number[]) {
-  //TODO Send SQL query to the DB
+  // TODO Send SQL query to the DB
   return;
+}
+
+export async function checkLogin(
+  pool: Pool,
+  meetingId: number,
+  user: string,
+  password: string
+) {
+  // TODO Check SQL
+  const [rows, _] = await pool.execute(
+    "SELECT * FROM host WHERE name = ? AND password = ? AND meeting_id = ?",
+    [user, password, meetingId]
+  );
+
+  if ((rows as any).length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
